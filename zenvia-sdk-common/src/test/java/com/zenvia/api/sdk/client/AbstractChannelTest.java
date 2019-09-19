7c4108bf5fe00710ed33545b25b51608cc824557
Rafael Souza <rafael@rafael.org>
2019-09-19 17:03:33 -0300

    Adding channel tests

---
+++ zenvia-sdk-common/src/test/java/com/zenvia/api/sdk/client/AbstractChannelTest.java
@@ -0,0 +1,86 @@
+package com.zenvia.api.sdk.client;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.mockito.Mockito;
+
+import com.zenvia.api.sdk.client.exceptions.HttpRequestException;
+import com.zenvia.api.sdk.client.exceptions.UnsuccessfulRequestException;
+import com.zenvia.api.sdk.client.exceptions.UnsupportedContentException;
+import com.zenvia.api.sdk.client.messages.MessageRequest;
+import com.zenvia.api.sdk.contents.Content;
+import com.zenvia.api.sdk.contents.TextContent;
+
+
+@FixMethodOrder( MethodSorters.NAME_ASCENDING )
+public abstract class AbstractChannelTest {
+	protected final IClient client = Mockito.mock( IClient.class );
+
+	protected final Channel channel;
+
+
+	public AbstractChannelTest( ChannelType channelType ) {
+		this.channel = new Channel( channelType, client );
+	}
+
+
+	@Before
+	public void before() {
+		when( client.getApiUrl() ).thenReturn( "URL" );
+	}
+
+
+	@Test
+	public void textContentSupported() {
+		contentSupportedTest( new TextContent( "This is a test!" ) );
+	}
+
+
+	@Test
+	public void multipleTextContentSupported() {
+		contentSupportedTest(
+			new TextContent( "This is the first test!" ),
+			new TextContent( "This is the second test!" ),
+			new TextContent( "This is the third test!" )
+		);
+	}
+
+
+	protected void contentSupportedTest( Content... contents ) {
+		try {
+			MessageRequest messageRequest = new MessageRequest( "from", "to", contents );
+			channel.sendMessage( messageRequest );
+			verify( client ).sendMessage( channel, messageRequest );
+		} catch( UnsuccessfulRequestException | HttpRequestException exception ) {
+			fail();
+		}
+	}
+
+
+	protected void contentNotSupportedTest( int expectedFailIndex, Content... contents ) {
+		try {
+			try {
+				channel.sendMessage( new MessageRequest( "from", "to", contents ) );
+				fail();
+			} catch( UnsupportedContentException exception ) {
+				assertEquals( contents[expectedFailIndex].type.name(), exception.getContentType() );
+				assertEquals( channel.type.name(), exception.getChannelType() );
+				assertEquals( "Content type " + exception.getContentType() + " is not supported by " + exception.getChannelType() + " channel", exception.getMessage() );
+				verify( client, never() ).sendMessage( any( Channel.class ), any( MessageRequest.class ) );
+			} catch( UnsuccessfulRequestException exception ) {
+				fail();
+			}
+		} catch( UnsuccessfulRequestException | HttpRequestException exception ) {
+			fail();
+		}
+	}
+}
---
+++ zenvia-sdk-common/src/test/java/com/zenvia/api/sdk/client/SmsChannelTest.java
@@ -0,0 +1,68 @@
+package com.zenvia.api.sdk.client;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import com.zenvia.api.sdk.contents.FileContent;
+import com.zenvia.api.sdk.contents.TemplateContent;
+import com.zenvia.api.sdk.contents.TextContent;
+
+
+@FixMethodOrder( MethodSorters.NAME_ASCENDING )
+public class SmsChannelTest extends AbstractChannelTest {
+	public SmsChannelTest() {
+		super( ChannelType.sms ); 
+	}
+
+
+	@Test
+	public void fileContentNotSupported() {
+		contentNotSupportedTest( 0, new FileContent(
+			"https://zenvia.com/favicon.ico",
+			"image/x-icon",
+			"This is a test!" 
+		) );
+	}
+
+
+	@Test
+	public void fileContentNotSupportedMixedWithSupportedContent() {
+		contentNotSupportedTest( 2,
+			new TextContent( "This is the first test!" ),
+			new TextContent( "This is the second test!" ),
+			new FileContent(
+				"https://zenvia.com/favicon.ico",
+				"image/x-icon",
+				"This is the third test!" 
+			),
+			new TextContent( "This is the fourth test!" )
+		);
+	}
+
+
+	@Test
+	public void templateContentNotSupported() {
+		Map<String,String> fields = new HashMap<String,String>();
+		fields.put( "name", "value" );
+		contentNotSupportedTest( 0, new TemplateContent( "12345", fields ) );
+	}
+
+
+	@Test
+	public void templateContentNotSupportedMixedWithSupportedContent() {
+		contentNotSupportedTest( 3,
+			new TextContent( "This is the first test!" ),
+			new TextContent( "This is the second test!" ),
+			new TextContent( "This is the third test!" ),
+			new FileContent(
+				"https://zenvia.com/favicon.ico",
+				"image/x-icon",
+				"This is the fourth test!" 
+			)
+		);
+	}
+}
