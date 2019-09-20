5db3e508aa85ea79951fab7a6bd872c60e07d29f
Rafael Souza <rafael@rafael.org>
2019-09-20 19:16:41 -0300

    Adding support to deserialization of MessageRequest for use on tests

--- zenvia-sdk-common/src/main/java/com/zenvia/api/sdk/client/messages/MessageRequest.java
+++ zenvia-sdk-common/src/main/java/com/zenvia/api/sdk/client/messages/MessageRequest.java
@@ -7,6 +7,9 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import com.zenvia.api.sdk.contents.Content;
 
 
@@ -28,7 +31,12 @@ public class MessageRequest {
 	}
 
 
-	public MessageRequest( String from, String to, List<Content> contents ) {
+	@JsonCreator
+	public MessageRequest(
+		@JsonProperty( "from" ) String from,
+		@JsonProperty( "to" ) String to,
+		@JsonProperty( "contents" ) List<Content> contents
+	) {
 		this.from = from;
 		this.to = to;
 		this.contents = Collections.unmodifiableList( contents );
