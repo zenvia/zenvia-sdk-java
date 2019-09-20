3ebb05d3091a179eed9c8d72301ddbc2fe7643c0
Rafael Souza <rafael@rafael.org>
2019-09-20 20:43:27 -0300

    Creating test for empty replies

--- zenvia-sdk-client-apache/src/test/java/com/zenvia/api/sdk/client/apache/ClientTest.java
+++ zenvia-sdk-client-apache/src/test/java/com/zenvia/api/sdk/client/apache/ClientTest.java
@@ -26,6 +26,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.zenvia.api.sdk.client.ChannelType;
 import com.zenvia.api.sdk.client.errors.ErrorResponse;
+import com.zenvia.api.sdk.client.exceptions.UnexpectedResponseBodyException;
 import com.zenvia.api.sdk.client.exceptions.UnsuccessfulRequestException;
 import com.zenvia.api.sdk.client.messages.MessageRequest;
 import com.zenvia.api.sdk.client.messages.MessageResponse;
@@ -48,6 +49,89 @@ public class ClientTest {
 	@Before
 	public void reset() {
 		server.lastBody = null;
+	}
+
+
+	@Test
+	public void unsuccessfulMessageRequest() {
+		Client client = new Client( "API_TOKEN", "http://127.0.0.1:" + serverPort );
+		try {
+			client.sendMessage( client.getChannel( "sms" ), messageRequest() );
+			fail();
+		} catch( UnsuccessfulRequestException exception ) {
+			assertNotNull( server.lastBody );
+			assertEquals( "from", server.lastBody.from );
+			assertEquals( "to", server.lastBody.to );
+			assertNotNull( server.lastBody.contents );
+			assertEquals( 1, server.lastBody.contents.size() );
+			assertEquals( ContentType.text, server.lastBody.contents.get( 0 ).type );
+			assertEquals( "This is a test!", ( (TextContent) server.lastBody.contents.get( 0 ) ).text );
+
+			ErrorResponse errorResponse = exception.getBody();
+			assertNotNull( errorResponse );
+			assertEquals( "TEST", errorResponse.code );
+			assertEquals( "This is a test!", errorResponse.message );
+			assertNotNull( errorResponse.details );
+			assertEquals( 1, errorResponse.details.size() );
+			assertEquals( "INVALID", errorResponse.details.get( 0 ).code );
+			assertEquals( "id", errorResponse.details.get( 0 ).path );
+			assertEquals( "Invalid id!", errorResponse.details.get( 0 ).message );
+		} catch( Exception exception ) {
+			fail();
+		}
+		client.close();
+	}
+
+
+	@Test
+	public void successfulJsonRequest() throws Exception {
+		Client client = new Client( "API_TOKEN", "http://127.0.0.1:" + serverPort );
+		MessageResponse messageResponse = client.sendMessage( client.getChannel( "whatsapp" ), messageRequest() );
+
+		assertNotNull( server.lastBody );
+		assertEquals( "from", server.lastBody.from );
+		assertEquals( "to", server.lastBody.to );
+		assertNotNull( server.lastBody.contents );
+		assertEquals( 1, server.lastBody.contents.size() );
+		assertEquals( ContentType.text, server.lastBody.contents.get( 0 ).type );
+		assertEquals( "This is a test!", ( (TextContent) server.lastBody.contents.get( 0 ) ).text );
+
+		assertNotNull( messageResponse );
+		assertEquals( "12345", messageResponse.id );
+		assertEquals( "123", messageResponse.from );
+		assertEquals( "456", messageResponse.to );
+		assertEquals( TMessageDirection.OUT, messageResponse.direction );
+		assertEquals( ChannelType.whatsapp, messageResponse.channel );
+		assertNotNull( messageResponse.contents );
+		assertEquals( 1, messageResponse.contents.size() );
+		assertEquals( ContentType.text, messageResponse.contents.get( 0 ).type );
+		assertEquals( "This is a test!", ( (TextContent) messageResponse.contents.get( 0 ) ).text );
+
+		client.close();
+	}
+
+
+	@Test
+	public void emptyReply() {
+		Client client = new Client( "API_TOKEN", "http://127.0.0.1:" + serverPort );
+		try {
+			client.sendMessage( client.getChannel( "facebook" ), messageRequest() );
+			fail();
+		} catch( UnexpectedResponseBodyException exception ) {
+			assertNotNull( server.lastBody );
+			assertEquals( "from", server.lastBody.from );
+			assertEquals( "to", server.lastBody.to );
+			assertNotNull( server.lastBody.contents );
+			assertEquals( 1, server.lastBody.contents.size() );
+			assertEquals( ContentType.text, server.lastBody.contents.get( 0 ).type );
+			assertEquals( "This is a test!", ( (TextContent) server.lastBody.contents.get( 0 ) ).text );
+
+			assertEquals( "", exception.getBody() );
+		} catch( Exception exception ) {
+			exception.printStackTrace();
+			fail();
+		}
+		client.close();
 	}
 
 
@@ -82,34 +166,6 @@ public class ClientTest {
 	}
 
 
-	@Test
-	public void successfulJsonRequestExecution() throws Exception {
-		Client client = new Client( "API_TOKEN", "http://127.0.0.1:" + serverPort );
-		MessageResponse messageResponse = client.sendMessage( client.getChannel( "whatsapp" ), messageRequest() );
-
-		assertNotNull( server.lastBody );
-		assertEquals( "from", server.lastBody.from );
-		assertEquals( "to", server.lastBody.to );
-		assertNotNull( server.lastBody.contents );
-		assertEquals( 1, server.lastBody.contents.size() );
-		assertEquals( ContentType.text, server.lastBody.contents.get( 0 ).type );
-		assertEquals( "This is a test!", ( (TextContent) server.lastBody.contents.get( 0 ) ).text );
-
-		assertNotNull( messageResponse );
-		assertEquals( "12345", messageResponse.id );
-		assertEquals( "123", messageResponse.from );
-		assertEquals( "456", messageResponse.to );
-		assertEquals( TMessageDirection.OUT, messageResponse.direction );
-		assertEquals( ChannelType.whatsapp, messageResponse.channel );
-		assertNotNull( messageResponse.contents );
-		assertEquals( 1, messageResponse.contents.size() );
-		assertEquals( ContentType.text, messageResponse.contents.get( 0 ).type );
-		assertEquals( "This is a test!", ( (TextContent) messageResponse.contents.get( 0 ) ).text );
-
-		client.close();
-	}
-
-
 	private MessageRequest messageRequest() {
 		return new MessageRequest( "from", "to", new TextContent( "This is a test!" ) ); 
 	}
@@ -135,6 +191,19 @@ public class ClientTest {
 			return Response
 				.status( 500 )
 				.entity( "{\"code\":\"TEST\",\"message\":\"This is a test!\",\"details\":[{\"code\":\"INVALID\",\"path\":\"id\",\"message\":\"Invalid id!\"}]}" )
+				.build();
+		}
+
+
+		@POST
+		@Path( "/v1/channels/facebook/messages" )
+		@Consumes( MediaType.APPLICATION_JSON )
+		@Produces( MediaType.APPLICATION_JSON )
+		public Response emptyResource( MessageRequest messageRequest ) {
+			this.lastBody = messageRequest;
+			return Response
+				.status( 500 )
+				.entity( "" )
 				.build();
 		}
 
