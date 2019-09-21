1a8c7b834df883a02b8fcec5ed8c49035bc03bc2
Rafael Souza <rafael@rafael.org>
2019-09-20 22:53:12 -0300

    Creating more Client tests

--- zenvia-sdk-client-apache/src/test/java/com/zenvia/api/sdk/client/apache/ClientTest.java
+++ zenvia-sdk-client-apache/src/test/java/com/zenvia/api/sdk/client/apache/ClientTest.java
@@ -2,10 +2,14 @@ package com.zenvia.api.sdk.client.apache;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
+
+import java.lang.reflect.Field;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -24,8 +28,12 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.zenvia.api.sdk.client.AbstractClient;
 import com.zenvia.api.sdk.client.ChannelType;
 import com.zenvia.api.sdk.client.errors.ErrorResponse;
+import com.zenvia.api.sdk.client.exceptions.HttpConnectionFailException;
+import com.zenvia.api.sdk.client.exceptions.HttpConnectionTimeoutException;
+import com.zenvia.api.sdk.client.exceptions.HttpSocketTimeoutException;
 import com.zenvia.api.sdk.client.exceptions.UnexpectedResponseBodyException;
 import com.zenvia.api.sdk.client.exceptions.UnsuccessfulRequestException;
 import com.zenvia.api.sdk.client.messages.MessageRequest;
@@ -44,11 +52,147 @@ public class ClientTest {
 
 	@Inject
 	private TestServer server;
+	
+	private final Field apiTokenField;
+
+
+	public ClientTest() throws Exception {
+		apiTokenField = AbstractClient.class.getDeclaredField( "apiToken" );
+		apiTokenField.setAccessible( true );
+	}
 
 
 	@Before
 	public void reset() {
 		server.lastBody = null;
+	}
+
+
+	@Test
+	public void constructor1() throws Exception {
+		Client client = new Client( "API_TOKEN" );
+		assertEquals( "https://api.zenvia.com", client.getApiUrl() );
+		assertEquals( "API_TOKEN", apiTokenField.get( client ) );
+		assertEquals( 25000, client.getConnectionTimeout() );
+		assertEquals( 60000, client.getSocketTimeout() );
+		assertEquals( 4, client.getMaxAutoRetries() );
+		assertEquals( 100, client.getMaxTotalConnections() );
+		assertEquals( 100, client.getMaxPerHostConnections() );
+		assertEquals( 0, client.getConnectionPoolTimeout() );
+		assertEquals( 5000, client.getCheckStaleConnectionAfterInactivityTime() );
+		client.close();
+	}
+
+
+	@Test
+	public void constructor4() throws Exception {
+		Client client = new Client( "API_TOKEN", 11, 12, 13 );
+		assertEquals( "https://api.zenvia.com", client.getApiUrl() );
+		assertEquals( "API_TOKEN", apiTokenField.get( client ) );
+		assertEquals( 11, client.getConnectionTimeout() );
+		assertEquals( 12, client.getSocketTimeout() );
+		assertEquals( 13, client.getMaxAutoRetries() );
+		assertEquals( 100, client.getMaxTotalConnections() );
+		assertEquals( 100, client.getMaxPerHostConnections() );
+		assertEquals( 0, client.getConnectionPoolTimeout() );
+		assertEquals( 5000, client.getCheckStaleConnectionAfterInactivityTime() );
+		client.close();
+	}
+
+
+	@Test
+	public void constructor5() throws Exception {
+		Client client = new Client( "API_TOKEN", 11, 12, 13, 14 );
+		assertEquals( "https://api.zenvia.com", client.getApiUrl() );
+		assertEquals( "API_TOKEN", apiTokenField.get( client ) );
+		assertEquals( 11, client.getConnectionTimeout() );
+		assertEquals( 12, client.getSocketTimeout() );
+		assertEquals( 13, client.getMaxAutoRetries() );
+		assertEquals( 14, client.getMaxTotalConnections() );
+		assertEquals( 14, client.getMaxPerHostConnections() );
+		assertEquals( 0, client.getConnectionPoolTimeout() );
+		assertEquals( 5000, client.getCheckStaleConnectionAfterInactivityTime() );
+		client.close();
+	}
+
+
+	@Test
+	public void constructor7() throws Exception {
+		Client client = new Client( "API_TOKEN", 11, 12, 13, 14, 15, 16 );
+		assertEquals( "https://api.zenvia.com", client.getApiUrl() );
+		assertEquals( "API_TOKEN", apiTokenField.get( client ) );
+		assertEquals( 11, client.getConnectionTimeout() );
+		assertEquals( 12, client.getSocketTimeout() );
+		assertEquals( 13, client.getMaxAutoRetries() );
+		assertEquals( 14, client.getMaxTotalConnections() );
+		assertEquals( 14, client.getMaxPerHostConnections() );
+		assertEquals( 15, client.getConnectionPoolTimeout() );
+		assertEquals( 16, client.getCheckStaleConnectionAfterInactivityTime() );
+		client.close();
+	}
+
+
+	@Test
+	public void constructorWithUrl1() throws Exception {
+		Client client = new Client( "API_TOKEN", "https://zenvia.com" );
+		assertEquals( "https://zenvia.com", client.getApiUrl() );
+		assertEquals( "API_TOKEN", apiTokenField.get( client ) );
+		assertEquals( 25000, client.getConnectionTimeout() );
+		assertEquals( 60000, client.getSocketTimeout() );
+		assertEquals( 4, client.getMaxAutoRetries() );
+		assertEquals( 100, client.getMaxTotalConnections() );
+		assertEquals( 100, client.getMaxPerHostConnections() );
+		assertEquals( 0, client.getConnectionPoolTimeout() );
+		assertEquals( 5000, client.getCheckStaleConnectionAfterInactivityTime() );
+		client.close();
+	}
+
+
+	@Test
+	public void constructorWithUrl4() throws Exception {
+		Client client = new Client( "API_TOKEN", "https://zenvia.com", 11, 12, 13 );
+		assertEquals( "https://zenvia.com", client.getApiUrl() );
+		assertEquals( "API_TOKEN", apiTokenField.get( client ) );
+		assertEquals( 11, client.getConnectionTimeout() );
+		assertEquals( 12, client.getSocketTimeout() );
+		assertEquals( 13, client.getMaxAutoRetries() );
+		assertEquals( 100, client.getMaxTotalConnections() );
+		assertEquals( 100, client.getMaxPerHostConnections() );
+		assertEquals( 0, client.getConnectionPoolTimeout() );
+		assertEquals( 5000, client.getCheckStaleConnectionAfterInactivityTime() );
+		client.close();
+	}
+
+
+	@Test
+	public void constructorWithUrl5() throws Exception {
+		Client client = new Client( "API_TOKEN", "https://zenvia.com", 11, 12, 13, 14 );
+		assertEquals( "https://zenvia.com", client.getApiUrl() );
+		assertEquals( "API_TOKEN", apiTokenField.get( client ) );
+		assertEquals( 11, client.getConnectionTimeout() );
+		assertEquals( 12, client.getSocketTimeout() );
+		assertEquals( 13, client.getMaxAutoRetries() );
+		assertEquals( 14, client.getMaxTotalConnections() );
+		assertEquals( 14, client.getMaxPerHostConnections() );
+		assertEquals( 0, client.getConnectionPoolTimeout() );
+		assertEquals( 5000, client.getCheckStaleConnectionAfterInactivityTime() );
+		client.close();
+	}
+
+
+	@Test
+	public void constructorWithUrl7() throws Exception {
+		Client client = new Client( "API_TOKEN", "https://zenvia.com", 11, 12, 13, 14, 15, 16 );
+		assertEquals( "https://zenvia.com", client.getApiUrl() );
+		assertEquals( "API_TOKEN", apiTokenField.get( client ) );
+		assertEquals( 11, client.getConnectionTimeout() );
+		assertEquals( 12, client.getSocketTimeout() );
+		assertEquals( 13, client.getMaxAutoRetries() );
+		assertEquals( 14, client.getMaxTotalConnections() );
+		assertEquals( 14, client.getMaxPerHostConnections() );
+		assertEquals( 15, client.getConnectionPoolTimeout() );
+		assertEquals( 16, client.getCheckStaleConnectionAfterInactivityTime() );
+		client.close();
 	}
 
 
@@ -76,6 +220,34 @@ public class ClientTest {
 			assertEquals( "INVALID", errorResponse.details.get( 0 ).code );
 			assertEquals( "id", errorResponse.details.get( 0 ).path );
 			assertEquals( "Invalid id!", errorResponse.details.get( 0 ).message );
+		} catch( Exception exception ) {
+			fail();
+		}
+		client.close();
+	}
+
+
+	@Test
+	public void wrongTokenMessageRequest() {
+		Client client = new Client( "INCORRECT_TOKEN", "http://127.0.0.1:" + serverPort );
+		try {
+			client.sendMessage( client.getChannel( "whatsapp" ), messageRequest() );
+			fail();
+		} catch( UnsuccessfulRequestException exception ) {
+			assertNotNull( server.lastBody );
+			assertEquals( "from", server.lastBody.from );
+			assertEquals( "to", server.lastBody.to );
+			assertNotNull( server.lastBody.contents );
+			assertEquals( 1, server.lastBody.contents.size() );
+			assertEquals( ContentType.text, server.lastBody.contents.get( 0 ).type );
+			assertEquals( "This is a test!", ( (TextContent) server.lastBody.contents.get( 0 ) ).text );
+
+			ErrorResponse errorResponse = exception.getBody();
+			assertNotNull( errorResponse );
+			assertEquals( "AUTHENTICATION_ERROR", errorResponse.code );
+			assertEquals( "No authorization token was found", errorResponse.message );
+			assertNotNull( errorResponse.details );
+			assertEquals( 0, errorResponse.details.size() );
 		} catch( Exception exception ) {
 			fail();
 		}
@@ -136,12 +308,44 @@ public class ClientTest {
 
 
 	@Test
-	public void unsuccessfulMessageRequestExecution() {
-		Client client = new Client( "API_TOKEN", "http://127.0.0.1:" + serverPort );
+	public void connectionRefused() {
+		Client client = new Client( "API_TOKEN", "http://127.0.0.1:8" );
 		try {
-			client.sendMessage( client.getChannel( "sms" ), messageRequest() );
+			client.sendMessage( client.getChannel( "whatsapp" ), messageRequest() );
 			fail();
-		} catch( UnsuccessfulRequestException exception ) {
+		} catch( HttpConnectionFailException exception ) {
+			assertNull( server.lastBody );
+		} catch( Exception exception ) {
+			exception.printStackTrace();
+			fail();
+		}
+		client.close();
+	}
+
+
+	@Test
+	public void connectionTimeout() {
+		Client client = new Client( "API_TOKEN", "http://192.168.255.253:8", 1000, 1000, null );
+		try {
+			client.sendMessage( client.getChannel( "whatsapp" ), messageRequest() );
+			fail();
+		} catch( HttpConnectionTimeoutException exception ) {
+			assertNull( server.lastBody );
+		} catch( Exception exception ) {
+			exception.printStackTrace();
+			fail();
+		}
+		client.close();
+	}
+
+
+	@Test
+	public void socketTimeout() {
+		Client client = new Client( "API_TOKEN", "http://127.0.0.1:" + serverPort + "/timeout", 1000, 1000, null );
+		try {
+			client.sendMessage( client.getChannel( "whatsapp" ), messageRequest() );
+			fail();
+		} catch( HttpSocketTimeoutException exception ) {
 			assertNotNull( server.lastBody );
 			assertEquals( "from", server.lastBody.from );
 			assertEquals( "to", server.lastBody.to );
@@ -149,17 +353,8 @@ public class ClientTest {
 			assertEquals( 1, server.lastBody.contents.size() );
 			assertEquals( ContentType.text, server.lastBody.contents.get( 0 ).type );
 			assertEquals( "This is a test!", ( (TextContent) server.lastBody.contents.get( 0 ) ).text );
-
-			ErrorResponse errorResponse = exception.getBody();
-			assertNotNull( errorResponse );
-			assertEquals( "TEST", errorResponse.code );
-			assertEquals( "This is a test!", errorResponse.message );
-			assertNotNull( errorResponse.details );
-			assertEquals( 1, errorResponse.details.size() );
-			assertEquals( "INVALID", errorResponse.details.get( 0 ).code );
-			assertEquals( "id", errorResponse.details.get( 0 ).path );
-			assertEquals( "Invalid id!", errorResponse.details.get( 0 ).message );
 		} catch( Exception exception ) {
+			exception.printStackTrace();
 			fail();
 		}
 		client.close();
@@ -179,6 +374,22 @@ public class ClientTest {
 		
 		public TestServer() {
 			register( this );
+		}
+
+
+		@POST
+		@Path( "/timeout/v1/channels/whatsapp/messages" )
+		@Consumes( MediaType.APPLICATION_JSON )
+		@Produces( MediaType.APPLICATION_JSON )
+		public Response timeoutResource( MessageRequest messageRequest ) {
+			this.lastBody = messageRequest;
+			try {
+				Thread.sleep( 5000 );
+			}
+			catch( InterruptedException exception ) {
+				throw new IllegalStateException( exception );
+			}
+			return null;
 		}
 
 
@@ -212,9 +423,21 @@ public class ClientTest {
 		@Path( "/v1/channels/whatsapp/messages" )
 		@Consumes( MediaType.APPLICATION_JSON )
 		@Produces( MediaType.APPLICATION_JSON )
-		public String messageResource( MessageRequest messageRequest ) {
+		public Response messageResource(
+			MessageRequest messageRequest,
+			@HeaderParam( "x-api-token" ) String token
+		) {
 			this.lastBody = messageRequest;
-			return "{\"id\":\"12345\",\"from\":\"123\",\"to\":\"456\",\"direction\":\"OUT\",\"channel\":\"whatsapp\",\"contents\":[{\"type\":\"text\",\"text\":\"This is a test!\"}]}";
+			if( token != null && token.equals( "API_TOKEN" ) ) {
+				return Response
+					.ok( "{\"id\":\"12345\",\"from\":\"123\",\"to\":\"456\",\"direction\":\"OUT\",\"channel\":\"whatsapp\",\"contents\":[{\"type\":\"text\",\"text\":\"This is a test!\"}]}" )
+					.build();
+			} else {
+				return Response
+					.status( 401 )
+					.entity( "{\"code\":\"AUTHENTICATION_ERROR\",\"message\":\"No authorization token was found\"}" )
+					.build();
+			}
 		}
 	}
 }
