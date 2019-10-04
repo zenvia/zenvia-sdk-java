b196e02a27648967640d77e819690a6eacf0a040
Rafael Souza <rafael@rafael.org>
2019-10-04 16:54:15 -0300

    Adding javadocs and small fix

--- zenvia-sdk-core/src/main/java/com/zenvia/api/sdk/contents/JsonContent.java
+++ zenvia-sdk-core/src/main/java/com/zenvia/api/sdk/contents/JsonContent.java
@@ -1,20 +1,30 @@
 package com.zenvia.api.sdk.contents;
 
-import java.util.HashMap;
+import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 
+/** Generic content used for multiple purposes. Usually used to receive metadata from the sender. 
+*
+*  @since 0.9.0 */
 public class JsonContent extends Content {
+	/** @since 0.9.0 */
 	public static final String TYPE = "json";
 
 
-	public final HashMap<String,Object> payload;
+	/** The payload object as a map.
+	 *
+	 *  @since 0.9.0 */
+	public final Map<String,Object> payload;
 
 
+	/** @param payload The payload object as a map.
+	 *
+	 *  @since 0.9.0 */
 	@JsonCreator
-	public JsonContent( @JsonProperty( "payload" ) HashMap<String,Object> payload ) {
+	public JsonContent( @JsonProperty( "payload" ) Map<String,Object> payload ) {
 		super( ContentType.json );
 		this.payload = payload;
 	}
